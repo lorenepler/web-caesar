d5632508c09e158627ae897d3be7788279326b35
Loren Epler <lepler@servicemanagement.com>
2017-01-26 16:28:56 -0600

    added validation

--- main.py
+++ main.py
@@ -16,11 +16,38 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    rot_label = "<label>Rotate by:</label>"
+    rotation_input = "<input type='number' name='rotation' />"
+
+    message_label = "<label>Type a message:</label>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+
+    submit = "<input type='submit' />"
+    form = ("<form method='post'>" +
+            rot_label + rotation_input + "<br>" +
+            message_label + textarea + "<br>" +
+            submit + "</form>")
+
+    header = "<h2>Web Caesar</h2>"
+
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message =
-        self.response.write('Hello web caesar!')
+        content = build_page("")
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message") #hello</textarea>Hello
+        rotation = int(self.request.get("rotation")) # 0
+        encrypted_message = caesar.encrypt(message, rotation) #hello</textarea>Hello
+        escaped_message = cgi.escape(encrypted_message) # hello&lt;/textarea&gt;hello
+        content = build_page(escaped_message)
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
